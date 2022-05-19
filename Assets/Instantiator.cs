e231e576be58b66667bac415129e19c39d455b9e
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-05-19 15:55:48 -0300

    Repaso Prueba

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject myCubePrefab;
+    public int cloneAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < cloneAmount; i++)
+            {
+                GameObject clon;
+                clon = Instantiate(myCubePrefab);
+                Destroy(clon, 1);
+            }
+        }
+    }
+}
